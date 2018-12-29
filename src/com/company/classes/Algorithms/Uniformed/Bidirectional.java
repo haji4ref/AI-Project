0f401ab76cf5f82ef92f9161e2b92aa41a7bba99
Aref <haji4ref@gmail.com>
2018-12-29 20:56:52 +0330

    @ Adds bidirectional algorithm class.

---
+++ src/com/company/classes/Algorithms/Uniformed/Bidirectional.java
@@ -0,0 +1,19 @@
+package com.company.classes.Algorithms.Uniformed;
+
+import com.company.classes.Algorithms.Algorithm;
+import com.company.classes.Problems.Problem;
+import com.company.classes.Problems.State;
+
+public class Bidirectional extends Algorithm {
+    public Bidirectional(Problem problem, State state) {
+        super(problem, state);
+
+        this.name = "bidirectional";
+    }
+
+
+    @Override
+    public Algorithm execute() {
+        return null;
+    }
+}
