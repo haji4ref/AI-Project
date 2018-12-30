205e25123e9ac771c53c08b3c8288e1b7aac0d59
Aref <haji4ref@gmail.com>
2018-12-30 14:48:40 +0330

    @ Adds some method to print path and lists.

--- src/com/company/classes/Printer.java
+++ src/com/company/classes/Printer.java
@@ -1,15 +1,33 @@
 package com.company.classes;
 
+import com.company.classes.Algorithms.Path;
 import com.company.classes.Problems.State;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Printer {
 
-    public static void printStatesIterator(Iterator<State> iterator) {
+    public static void print(Iterator<State> iterator) {
         while (iterator.hasNext())
             System.out.println(iterator.next().joinValues() + " ");
         System.out.println("------------");
 
     }
+
+    public static void printPath(Iterator<Path> iterator) {
+        while (iterator.hasNext())
+            iterator.next().printPath();
+        System.out.println("------------");
+
+    }
+
+    public static void print(ArrayList<String> arrayList) {
+        int counter = 0;
+        while (counter < arrayList.size())
+            System.out.println(arrayList.get(counter++) + " ");
+        System.out.println("------------");
+    }
+
+
 }
